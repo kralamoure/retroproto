5b54283d1a2efc34c7ace9bf6cb2428a8ec2b09b
Raúl Garrido <me@happybydefault.com>
2020-07-09 07:20:52 -0400

    early development

--- msgsvr/exchangebigstoreitemslist.go
+++ msgsvr/exchangebigstoreitemslist.go
@@ -1,54 +1,20 @@
+// generated (unrevised)
 package msgsvr
 
 import (
-	"fmt"
-	"strconv"
-	"strings"
-
 	"github.com/kralamoure/d1proto"
 )
 
-type ExchangeBigStoreItemsList struct {
-	ItemType        int
-	ItemTemplateIds []int
-}
+type ExchangeBigStoreItemsList struct{}
 
 func (m ExchangeBigStoreItemsList) ProtocolId() d1proto.MsgSvrId {
 	return d1proto.ExchangeBigStoreItemsList
 }
 
 func (m ExchangeBigStoreItemsList) Serialized() (string, error) {
-	itemTemplateIds := make([]string, len(m.ItemTemplateIds))
-	for i, v := range m.ItemTemplateIds {
-		itemTemplateIds[i] = fmt.Sprintf("%d", v)
-	}
-
-	return fmt.Sprintf("%d|%s", m.ItemType, strings.Join(itemTemplateIds, ";")), nil
+	return "", nil
 }
 
 func (m *ExchangeBigStoreItemsList) Deserialize(extra string) error {
-	sli := strings.Split(extra, "|")
-	if len(sli) != 2 {
-		return d1proto.ErrInvalidMsg
-	}
-
-	itemType, err := strconv.ParseInt(sli[0], 10, 32)
-	if err != nil {
-		return err
-	}
-	m.ItemType = int(itemType)
-
-	if sli[1] != "" {
-		itemTemplateIds := strings.Split(sli[1], ";")
-		m.ItemTemplateIds = make([]int, len(itemTemplateIds))
-		for i, v := range itemTemplateIds {
-			itemTemplateId, err := strconv.ParseInt(v, 10, 32)
-			if err != nil {
-				return err
-			}
-			m.ItemTemplateIds[i] = int(itemTemplateId)
-		}
-	}
-
 	return nil
 }
