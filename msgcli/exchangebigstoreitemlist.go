93d61cc349a1292c24191a67ab1a8e9926c54888
Raúl Garrido <me@happybydefault.com>
2020-07-09 19:43:01 -0400

    early development

--- msgcli/exchangebigstoreitemlist.go
+++ msgcli/exchangebigstoreitemlist.go
@@ -1,20 +1,29 @@
-// generated (unrevised)
 package msgcli
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/kralamoure/d1proto"
 )
 
-type ExchangeBigStoreItemList struct{}
+type ExchangeBigStoreItemList struct {
+	ItemTemplateId int
+}
 
 func (m ExchangeBigStoreItemList) ProtocolId() d1proto.MsgCliId {
 	return d1proto.ExchangeBigStoreItemList
 }
 
 func (m ExchangeBigStoreItemList) Serialized() (string, error) {
-	return "", nil
+	return fmt.Sprintf("%d", m.ItemTemplateId), nil
 }
 
 func (m *ExchangeBigStoreItemList) Deserialize(extra string) error {
+	itemTemplateId, err := strconv.ParseInt(extra, 10, 32)
+	if err != nil {
+		return err
+	}
+	m.ItemTemplateId = int(itemTemplateId)
 	return nil
 }
