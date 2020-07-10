e8d83f4fc325b037b89d4a6c75fa1d0d163ac65a
Raúl Garrido <me@happybydefault.com>
2020-07-10 18:47:44 -0400

    early development

--- msgcli/dialogbeginning.go
+++ msgcli/dialogbeginning.go
@@ -1,20 +1,29 @@
-// generated (unrevised)
 package msgcli
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/kralamoure/d1proto"
 )
 
-type DialogBeginning struct{}
+type DialogBeginning struct {
+	NPCId int
+}
 
 func (m DialogBeginning) ProtocolId() d1proto.MsgCliId {
 	return d1proto.DialogBeginning
 }
 
 func (m DialogBeginning) Serialized() (string, error) {
-	return "", nil
+	return fmt.Sprint(m.NPCId), nil
 }
 
 func (m *DialogBeginning) Deserialize(extra string) error {
+	npcId, err := strconv.ParseInt(extra, 10, 32)
+	if err != nil {
+		return err
+	}
+	m.NPCId = int(npcId)
 	return nil
 }
--- msgcli/dialogresponse.go
+++ msgcli/dialogresponse.go
@@ -1,20 +1,43 @@
-// generated (unrevised)
 package msgcli
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
+
 	"github.com/kralamoure/d1proto"
 )
 
-type DialogResponse struct{}
+type DialogResponse struct {
+	Question int
+	Answer   int
+}
 
 func (m DialogResponse) ProtocolId() d1proto.MsgCliId {
 	return d1proto.DialogResponse
 }
 
 func (m DialogResponse) Serialized() (string, error) {
-	return "", nil
+	return fmt.Sprintf("%d|%d", m.Question, m.Answer), nil
 }
 
 func (m *DialogResponse) Deserialize(extra string) error {
+	sli := strings.Split(extra, "|")
+	if len(sli) != 2 {
+		return d1proto.ErrInvalidMsg
+	}
+
+	question, err := strconv.ParseInt(sli[0], 10, 32)
+	if err != nil {
+		return err
+	}
+	m.Question = int(question)
+
+	answer, err := strconv.ParseInt(sli[1], 10, 32)
+	if err != nil {
+		return err
+	}
+	m.Answer = int(answer)
+
 	return nil
 }
