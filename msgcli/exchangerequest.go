22f22b62f7571a56d6ec143ca42efa80340f9070
Raúl Garrido <me@happybydefault.com>
2020-07-13 19:12:18 -0400

    early development

--- msgcli/exchangerequest.go
+++ msgcli/exchangerequest.go
@@ -1,20 +1,66 @@
-// generated (unrevised)
 package msgcli
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
+
 	"github.com/kralamoure/d1proto"
 )
 
-type ExchangeRequest struct{}
+type ExchangeRequest struct {
+	Type int
+	Id   int
+	Cell int
+}
 
 func (m ExchangeRequest) ProtocolId() d1proto.MsgCliId {
 	return d1proto.ExchangeRequest
 }
 
 func (m ExchangeRequest) Serialized() (string, error) {
-	return "", nil
+	var id string
+	if m.Id != 0 {
+		id = fmt.Sprintf("%d", m.Id)
+	}
+
+	var cell string
+	if m.Cell != 0 {
+		id = fmt.Sprintf("|%d", m.Cell)
+	}
+
+	return fmt.Sprintf("%d|%s%s", m.Type, id, cell), nil
 }
 
 func (m *ExchangeRequest) Deserialize(extra string) error {
+
+	sli := strings.Split(extra, "|")
+
+	if len(sli) < 2 {
+		return d1proto.ErrInvalidMsg
+	}
+
+	requestType, err := strconv.ParseInt(sli[0], 10, 32)
+	if err != nil {
+		return err
+	}
+	m.Type = int(requestType)
+
+	if sli[1] != "" {
+		id, err := strconv.ParseInt(sli[1], 10, 32)
+		if err != nil {
+			return err
+		}
+		m.Id = int(id)
+	}
+
+	if len(sli) >= 3 && sli[2] != "" {
+		cell, err := strconv.ParseInt(sli[2], 10, 32)
+		if err != nil {
+			return err
+		}
+		m.Cell = int(cell)
+	}
+
 	return nil
 }
