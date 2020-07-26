21e3aeb22dc35758642bd6895ce52204b574ddd0
Raúl Garrido <me@happybydefault.com>
2020-07-26 14:24:21 -0400

    early development

--- msgsvr/itemsremove.go
+++ msgsvr/itemsremove.go
@@ -1,20 +1,34 @@
-// generated (unrevised)
 package msgsvr
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/kralamoure/d1proto"
 )
 
-type ItemsRemove struct{}
+type ItemsRemove struct {
+	Id int
+}
 
 func (m ItemsRemove) ProtocolId() d1proto.MsgSvrId {
 	return d1proto.ItemsRemove
 }
 
 func (m ItemsRemove) Serialized() (string, error) {
-	return "", nil
+	return fmt.Sprintf("%d", m.Id), nil
 }
 
 func (m *ItemsRemove) Deserialize(extra string) error {
+	if extra == "" {
+		return d1proto.ErrInvalidMsg
+	}
+
+	id, err := strconv.ParseInt(extra, 10, 32)
+	if err != nil {
+		return err
+	}
+	m.Id = int(id)
+
 	return nil
 }
