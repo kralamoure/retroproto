6b0f083a1c4e385b9ac1501bb9d8b7f843ee2051
Raúl Garrido <me@happybydefault.com>
2020-07-26 12:31:22 -0400

    early development

--- msgsvr/itemsadderror.go
+++ msgsvr/itemsadderror.go
@@ -1,20 +1,30 @@
-// generated (unrevised)
 package msgsvr
 
 import (
 	"github.com/kralamoure/d1proto"
 )
 
-type ItemsAddError struct{}
+type ItemsAddError struct {
+	Reason rune
+}
 
 func (m ItemsAddError) ProtocolId() d1proto.MsgSvrId {
 	return d1proto.ItemsAddError
 }
 
 func (m ItemsAddError) Serialized() (string, error) {
-	return "", nil
+	return string(m.Reason), nil
 }
 
 func (m *ItemsAddError) Deserialize(extra string) error {
+	if len(extra) == 0 {
+		return d1proto.ErrInvalidMsg
+	}
+
+	for _, v := range extra {
+		m.Reason = v
+		break
+	}
+
 	return nil
 }
