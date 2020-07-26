53585e37ce47bbc0061fa5fac4f47d8319b07228
Raúl Garrido <me@happybydefault.com>
2020-07-26 19:17:26 -0400

    early development

--- typ/accountcharacterselectedsuccessitems.go
+++ typ/accountcharacterselectedsuccessitems.go
@@ -2,18 +2,22 @@ package typ
 
 import (
 	"fmt"
+	"strings"
+
+	"github.com/kralamoure/d1"
+	"github.com/kralamoure/d1/d1typ"
 )
 
 type AccountCharacterSelectedSuccessItem struct {
 	Id         int
 	TemplateId int
 	Qty        int
-	Position   int // -1 for no position
-	Effects    string
+	Position   d1typ.CharacterItemPosition
+	Effects    []d1typ.Effect
 }
 
 func (m AccountCharacterSelectedSuccessItem) Serialized() (string, error) {
-	return fmt.Sprintf("%x~%x~%x~%x~%s", m.Id, m.TemplateId, m.Qty, m.Position, m.Effects), nil
+	return fmt.Sprintf("%x~%x~%x~%x~%s", m.Id, m.TemplateId, m.Qty, m.Position, strings.Join(d1.EncodeItemEffects(m.Effects), ",")), nil
 }
 
 // TODO
