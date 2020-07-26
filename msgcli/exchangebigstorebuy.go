5ba4b9f39a2df602973ba418c6a2cb82693101d0
Raúl Garrido <me@happybydefault.com>
2020-07-26 00:43:53 -0400

    early development

--- msgcli/exchangebigstorebuy.go
+++ msgcli/exchangebigstorebuy.go
@@ -1,20 +1,50 @@
-// generated (unrevised)
 package msgcli
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
+
 	"github.com/kralamoure/d1proto"
 )
 
-type ExchangeBigStoreBuy struct{}
+type ExchangeBigStoreBuy struct {
+	ItemId        int
+	QuantityIndex int
+	Price         int
+}
 
 func (m ExchangeBigStoreBuy) ProtocolId() d1proto.MsgCliId {
 	return d1proto.ExchangeBigStoreBuy
 }
 
 func (m ExchangeBigStoreBuy) Serialized() (string, error) {
-	return "", nil
+	return fmt.Sprintf("%d|%d|%d", m.ItemId, m.QuantityIndex, m.Price), nil
 }
 
 func (m *ExchangeBigStoreBuy) Deserialize(extra string) error {
+	sli := strings.Split(extra, "|")
+	if len(sli) != 3 {
+		return d1proto.ErrInvalidMsg
+	}
+
+	itemId, err := strconv.ParseInt(sli[0], 10, 32)
+	if err != nil {
+		return err
+	}
+	m.ItemId = int(itemId)
+
+	quantityIndex, err := strconv.ParseInt(sli[1], 10, 32)
+	if err != nil {
+		return err
+	}
+	m.QuantityIndex = int(quantityIndex)
+
+	price, err := strconv.ParseInt(sli[2], 10, 32)
+	if err != nil {
+		return err
+	}
+	m.Price = int(price)
+
 	return nil
 }
