eb4f9b8b4b74445830a6b7c4667cadb3455aaebc
Raúl Garrido <me@happybydefault.com>
2020-07-26 00:19:18 -0400

    early development

--- msgcli/exchangegetitemmiddlepriceinbigstore.go
+++ msgcli/exchangegetitemmiddlepriceinbigstore.go
@@ -1,20 +1,30 @@
-// generated (unrevised)
 package msgcli
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/kralamoure/d1proto"
 )
 
-type ExchangeGetItemMiddlePriceInBigStore struct{}
+type ExchangeGetItemMiddlePriceInBigStore struct {
+	TemplateId int
+}
 
 func (m ExchangeGetItemMiddlePriceInBigStore) ProtocolId() d1proto.MsgCliId {
 	return d1proto.ExchangeGetItemMiddlePriceInBigStore
 }
 
 func (m ExchangeGetItemMiddlePriceInBigStore) Serialized() (string, error) {
-	return "", nil
+	return fmt.Sprintf("%d", m.TemplateId), nil
 }
 
 func (m *ExchangeGetItemMiddlePriceInBigStore) Deserialize(extra string) error {
+	templateId, err := strconv.ParseInt(extra, 10, 32)
+	if err != nil {
+		return err
+	}
+	m.TemplateId = int(templateId)
+
 	return nil
 }
