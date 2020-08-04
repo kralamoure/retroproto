314c9900dfb92a319521e7b7719ebce17fcb27a1
Raúl Garrido <me@happybydefault.com>
2020-08-04 15:59:41 -0400

    early development

--- msgsvr/itemsaccessories.go
+++ msgsvr/itemsaccessories.go
@@ -1,20 +1,48 @@
-// generated (unrevised)
 package msgsvr
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
+
 	"github.com/kralamoure/d1proto"
+	"github.com/kralamoure/d1proto/typ"
 )
 
-type ItemsAccessories struct{}
+type ItemsAccessories struct {
+	Id          int
+	Accessories typ.CommonAccessories
+}
 
 func (m ItemsAccessories) ProtocolId() d1proto.MsgSvrId {
 	return d1proto.ItemsAccessories
 }
 
 func (m ItemsAccessories) Serialized() (string, error) {
-	return "", d1proto.ErrNotImplemented
+	accessories, err := m.Accessories.Serialized()
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%d|%s", m.Id, accessories), nil
 }
 
 func (m *ItemsAccessories) Deserialize(extra string) error {
-	return d1proto.ErrNotImplemented
+	sli := strings.Split(extra, "|")
+	if len(sli) < 2 {
+		return d1proto.ErrInvalidMsg
+	}
+
+	id, err := strconv.ParseInt(sli[0], 10, 32)
+	if err != nil {
+		return err
+	}
+	m.Id = int(id)
+
+	err = m.Accessories.Deserialize(sli[1])
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
