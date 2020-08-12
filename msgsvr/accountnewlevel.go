e6f09b8f3ba26ceb6c8e9c9fdd37aa3a2c96a02b
Raúl Garrido <me@happybydefault.com>
2020-08-12 02:13:39 -0400

    early development

--- msgsvr/accountnewlevel.go
+++ msgsvr/accountnewlevel.go
@@ -1,20 +1,30 @@
-// generated (unrevised)
 package msgsvr
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/kralamoure/d1proto"
 )
 
-type AccountNewLevel struct{}
+type AccountNewLevel struct {
+	Level int
+}
 
 func (m AccountNewLevel) ProtocolId() d1proto.MsgSvrId {
 	return d1proto.AccountNewLevel
 }
 
 func (m AccountNewLevel) Serialized() (string, error) {
-	return "", d1proto.ErrNotImplemented
+	return fmt.Sprintf("%d", m.Level), nil
 }
 
 func (m *AccountNewLevel) Deserialize(extra string) error {
-	return d1proto.ErrNotImplemented
+	level, err := strconv.ParseInt(extra, 10, 32)
+	if err != nil {
+		return err
+	}
+	m.Level = int(level)
+
+	return nil
 }
