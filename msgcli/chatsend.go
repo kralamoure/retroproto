2b4306a56a86e6819d1daa008de04031b1994e8f
Raúl Garrido <me@happybydefault.com>
2020-08-11 22:47:07 -0400

    early development

--- msgcli/chatsend.go
+++ msgcli/chatsend.go
@@ -1,20 +1,33 @@
-// generated (unrevised)
 package msgcli
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/kralamoure/d1proto"
 )
 
-type ChatSend struct{}
+type ChatSend struct {
+	Destination string
+	Message     string
+}
 
 func (m ChatSend) ProtocolId() d1proto.MsgCliId {
 	return d1proto.ChatSend
 }
 
 func (m ChatSend) Serialized() (string, error) {
-	return "", d1proto.ErrNotImplemented
+	return fmt.Sprintf("%s|%s", m.Destination, m.Message), nil
 }
 
 func (m *ChatSend) Deserialize(extra string) error {
-	return d1proto.ErrNotImplemented
+	sli := strings.SplitN(extra, "|", 2)
+	if len(sli) != 2 {
+		return d1proto.ErrInvalidMsg
+	}
+
+	m.Destination = sli[0]
+	m.Message = sli[1]
+
+	return nil
 }
