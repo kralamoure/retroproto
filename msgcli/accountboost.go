7f08d70afec640309f58563c9c36560cd7328fb4
Raúl Garrido <me@happybydefault.com>
2020-08-16 01:09:04 -0400

    early development

--- msgcli/accountboost.go
+++ msgcli/accountboost.go
@@ -1,20 +1,31 @@
-// generated (unrevised)
 package msgcli
 
 import (
+	"fmt"
+	"strconv"
+
+	"github.com/kralamoure/d1/d1typ"
+
 	"github.com/kralamoure/d1proto"
 )
 
-type AccountBoost struct{}
+type AccountBoost struct {
+	CharacteristicId d1typ.CharacteristicId
+}
 
 func (m AccountBoost) ProtocolId() d1proto.MsgCliId {
 	return d1proto.AccountBoost
 }
 
 func (m AccountBoost) Serialized() (string, error) {
-	return "", d1proto.ErrNotImplemented
+	return fmt.Sprintf("%d", m.CharacteristicId), nil
 }
 
 func (m *AccountBoost) Deserialize(extra string) error {
-	return d1proto.ErrNotImplemented
+	characteristicId, err := strconv.ParseInt(extra, 10, 32)
+	if err != nil {
+		return err
+	}
+	m.CharacteristicId = d1typ.CharacteristicId(characteristicId)
+	return nil
 }
