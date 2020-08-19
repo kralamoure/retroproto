10a85c0cc175336d7457e8977404c00988d2d7b8
Raúl Garrido <me@happybydefault.com>
2020-08-18 20:39:44 -0400

    early development

--- msgcli/spellsboost.go
+++ msgcli/spellsboost.go
@@ -1,20 +1,30 @@
-// generated (unrevised)
 package msgcli
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/kralamoure/d1proto"
 )
 
-type SpellsBoost struct{}
+type SpellsBoost struct {
+	Id int
+}
 
 func (m SpellsBoost) ProtocolId() d1proto.MsgCliId {
 	return d1proto.SpellsBoost
 }
 
 func (m SpellsBoost) Serialized() (string, error) {
-	return "", d1proto.ErrNotImplemented
+	return fmt.Sprintf("%d", m.Id), nil
 }
 
 func (m *SpellsBoost) Deserialize(extra string) error {
-	return d1proto.ErrNotImplemented
+	id, err := strconv.ParseInt(extra, 10, 32)
+	if err != nil {
+		return err
+	}
+	m.Id = int(id)
+
+	return nil
 }
