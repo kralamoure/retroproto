3f495230ebe701583ff2414a77bd13eaa2eedcc4
Raúl Garrido <me@happybydefault.com>
2020-08-20 19:28:23 -0400

    early development

--- msgcli/itemsuseconfirm.go
+++ msgcli/itemsuseconfirm.go
@@ -1,20 +1,61 @@
-// generated (unrevised)
 package msgcli
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
+
 	"github.com/kralamoure/d1proto"
 )
 
-type ItemsUseConfirm struct{}
+type ItemsUseConfirm struct {
+	Id       int
+	SpriteId int
+	Cell     int
+}
 
 func (m ItemsUseConfirm) ProtocolId() d1proto.MsgCliId {
 	return d1proto.ItemsUseConfirm
 }
 
 func (m ItemsUseConfirm) Serialized() (string, error) {
-	return "", d1proto.ErrNotImplemented
+	spriteId := ""
+	if m.SpriteId != 0 {
+		spriteId = fmt.Sprintf("%d", m.SpriteId)
+	}
+
+	cell := ""
+	if m.Cell != 0 {
+		cell = fmt.Sprintf("|%d", m.Cell)
+	}
+
+	return fmt.Sprintf("%d|%s%s", m.Id, spriteId, cell), nil
 }
 
 func (m *ItemsUseConfirm) Deserialize(extra string) error {
-	return d1proto.ErrNotImplemented
+	sli := strings.Split(extra, "|")
+
+	id, err := strconv.ParseInt(sli[0], 10, 32)
+	if err != nil {
+		return err
+	}
+	m.Id = int(id)
+
+	if len(sli) >= 2 && sli[1] != "" {
+		spriteId, err := strconv.ParseInt(sli[1], 10, 32)
+		if err != nil {
+			return err
+		}
+		m.SpriteId = int(spriteId)
+	}
+
+	if len(sli) >= 3 && sli[2] != "" {
+		cell, err := strconv.ParseInt(sli[2], 10, 32)
+		if err != nil {
+			return err
+		}
+		m.Cell = int(cell)
+	}
+
+	return nil
 }
--- msgcli/itemsusenoconfirm.go
+++ msgcli/itemsusenoconfirm.go
@@ -1,20 +1,61 @@
-// generated (unrevised)
 package msgcli
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
+
 	"github.com/kralamoure/d1proto"
 )
 
-type ItemsUseNoConfirm struct{}
+type ItemsUseNoConfirm struct {
+	Id       int
+	SpriteId int
+	Cell     int
+}
 
 func (m ItemsUseNoConfirm) ProtocolId() d1proto.MsgCliId {
 	return d1proto.ItemsUseNoConfirm
 }
 
 func (m ItemsUseNoConfirm) Serialized() (string, error) {
-	return "", d1proto.ErrNotImplemented
+	spriteId := ""
+	if m.SpriteId != 0 {
+		spriteId = fmt.Sprintf("%d", m.SpriteId)
+	}
+
+	cell := ""
+	if m.Cell != 0 {
+		cell = fmt.Sprintf("|%d", m.Cell)
+	}
+
+	return fmt.Sprintf("%d|%s%s", m.Id, spriteId, cell), nil
 }
 
 func (m *ItemsUseNoConfirm) Deserialize(extra string) error {
-	return d1proto.ErrNotImplemented
+	sli := strings.Split(extra, "|")
+
+	id, err := strconv.ParseInt(sli[0], 10, 32)
+	if err != nil {
+		return err
+	}
+	m.Id = int(id)
+
+	if len(sli) >= 2 && sli[1] != "" {
+		spriteId, err := strconv.ParseInt(sli[1], 10, 32)
+		if err != nil {
+			return err
+		}
+		m.SpriteId = int(spriteId)
+	}
+
+	if len(sli) >= 3 && sli[2] != "" {
+		cell, err := strconv.ParseInt(sli[2], 10, 32)
+		if err != nil {
+			return err
+		}
+		m.Cell = int(cell)
+	}
+
+	return nil
 }
