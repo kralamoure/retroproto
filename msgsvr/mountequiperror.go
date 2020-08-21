329f3e849fa26c4dd27c660f998547f0866f53d6
Raúl Garrido <me@happybydefault.com>
2020-08-21 19:01:22 -0400

    early development

--- msgsvr/mountequiperror.go
+++ msgsvr/mountequiperror.go
@@ -4,16 +4,23 @@ import (
 	"github.com/kralamoure/d1proto"
 )
 
-type MountEquipError struct{}
+type MountEquipError struct {
+	Reason rune
+}
 
 func (m MountEquipError) ProtocolId() d1proto.MsgSvrId {
 	return d1proto.MountEquipError
 }
 
 func (m MountEquipError) Serialized() (string, error) {
-	return "", nil
+	return string(m.Reason), nil
 }
 
 func (m *MountEquipError) Deserialize(extra string) error {
+	for _, reason := range extra {
+		m.Reason = reason
+		break
+	}
+
 	return nil
 }
