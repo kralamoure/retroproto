86d785298895ed86f3816bf50d4b84d2a7d5fa72
Raúl Garrido <me@happybydefault.com>
2020-08-25 02:03:31 -0400

    early development

--- msgcli/mountrequestdata.go
+++ msgcli/mountrequestdata.go
@@ -1,20 +1,48 @@
-// generated (unrevised)
 package msgcli
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
 	"github.com/kralamoure/d1proto"
 )
 
-type MountRequestData struct{}
+type MountRequestData struct {
+	Id   int
+	Time time.Time
+}
 
 func (m MountRequestData) ProtocolId() d1proto.MsgCliId {
 	return d1proto.MountRequestData
 }
 
 func (m MountRequestData) Serialized() (string, error) {
-	return "", d1proto.ErrNotImplemented
+	return fmt.Sprintf("%d|%d", m.Id, m.Time.Unix()*1000), nil
 }
 
 func (m *MountRequestData) Deserialize(extra string) error {
-	return d1proto.ErrNotImplemented
+	sli := strings.Split(extra, "|")
+	if len(sli) != 2 {
+		return d1proto.ErrInvalidMsg
+	}
+
+	if sli[0] != "undefined" {
+		id, err := strconv.ParseInt(sli[0], 10, 32)
+		if err != nil {
+			return err
+		}
+		m.Id = int(id)
+	}
+
+	if sli[1] != "undefined" {
+		sec, err := strconv.ParseInt(sli[1], 10, 32)
+		if err != nil {
+			return err
+		}
+		m.Time = time.Unix(sec, 0)
+	}
+
+	return nil
 }
