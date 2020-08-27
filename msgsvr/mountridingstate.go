f649734130f750f6cbcfe24c6da59adcc1a1bf66
Raúl Garrido <me@happybydefault.com>
2020-08-26 22:58:10 -0400

    early development

--- msgsvr/mountridingstate.go
+++ msgsvr/mountridingstate.go
@@ -1,20 +1,37 @@
-// generated (unrevised)
 package msgsvr
 
 import (
 	"github.com/kralamoure/d1proto"
 )
 
-type MountRidingState struct{}
+type MountRidingState struct {
+	Riding bool
+}
 
 func (m MountRidingState) ProtocolId() d1proto.MsgSvrId {
 	return d1proto.MountRidingState
 }
 
 func (m MountRidingState) Serialized() (string, error) {
-	return "", d1proto.ErrNotImplemented
+	var riding string
+	if m.Riding {
+		riding = "+"
+	} else {
+		riding = "-"
+	}
+
+	return riding, nil
 }
 
 func (m *MountRidingState) Deserialize(extra string) error {
-	return d1proto.ErrNotImplemented
+	switch extra {
+	case "+":
+		m.Riding = true
+	case "-":
+		m.Riding = false
+	default:
+		return d1proto.ErrInvalidMsg
+	}
+
+	return nil
 }
