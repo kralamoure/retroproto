350215a0e05a3c567586b71b7761bd1c0bfa1131
Raúl Garrido <me@happybydefault.com>
2020-08-27 18:58:47 -0400

    early development

--- msgcli/gameactionack.go
+++ msgcli/gameactionack.go
@@ -1,20 +1,30 @@
-// generated (unrevised)
 package msgcli
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/kralamoure/d1proto"
 )
 
-type GameActionAck struct{}
+type GameActionAck struct {
+	Id int
+}
 
 func (m GameActionAck) ProtocolId() d1proto.MsgCliId {
 	return d1proto.GameActionAck
 }
 
 func (m GameActionAck) Serialized() (string, error) {
-	return "", d1proto.ErrNotImplemented
+	return fmt.Sprintf("%d", m.Id), nil
 }
 
 func (m *GameActionAck) Deserialize(extra string) error {
-	return d1proto.ErrNotImplemented
+	id, err := strconv.ParseInt(extra, 10, 32)
+	if err != nil {
+		return err
+	}
+	m.Id = int(id)
+
+	return nil
 }
