3ec760e757e1fd41fc975c1ea4c0de091fbe3cbb
Raúl Garrido <me@happybydefault.com>
2020-10-06 19:36:33 -0300

    early development

--- msgcli/exchangeputinshedfromcertificate.go
+++ msgcli/exchangeputinshedfromcertificate.go
@@ -1,20 +1,30 @@
-// generated (unrevised)
 package msgcli
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/kralamoure/d1proto"
 )
 
-type ExchangePutInShedFromCertificate struct{}
+type ExchangePutInShedFromCertificate struct {
+	CertificateId int
+}
 
 func (m ExchangePutInShedFromCertificate) ProtocolId() d1proto.MsgCliId {
 	return d1proto.ExchangePutInShedFromCertificate
 }
 
 func (m ExchangePutInShedFromCertificate) Serialized() (string, error) {
-	return "", d1proto.ErrNotImplemented
+	return fmt.Sprint(m.CertificateId), nil
 }
 
 func (m *ExchangePutInShedFromCertificate) Deserialize(extra string) error {
-	return d1proto.ErrNotImplemented
+	certificateId, err := strconv.Atoi(extra)
+	if err != nil {
+		return err
+	}
+	m.CertificateId = certificateId
+
+	return nil
 }
