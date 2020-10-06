926f30124c4316b4708481600a462065119d92b7
Raúl Garrido <me@happybydefault.com>
2020-10-06 20:09:57 -0300

    early development

--- msgsvr/exchangemountstorageremove.go
+++ msgsvr/exchangemountstorageremove.go
@@ -1,20 +1,30 @@
-// generated (unrevised)
 package msgsvr
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/kralamoure/d1proto"
 )
 
-type ExchangeMountStorageRemove struct{}
+type ExchangeMountStorageRemove struct {
+	MountId int
+}
 
 func (m ExchangeMountStorageRemove) ProtocolId() d1proto.MsgSvrId {
 	return d1proto.ExchangeMountStorageRemove
 }
 
 func (m ExchangeMountStorageRemove) Serialized() (string, error) {
-	return "", d1proto.ErrNotImplemented
+	return fmt.Sprint(m.MountId), nil
 }
 
 func (m *ExchangeMountStorageRemove) Deserialize(extra string) error {
-	return d1proto.ErrNotImplemented
+	mountId, err := strconv.Atoi(extra)
+	if err != nil {
+		return err
+	}
+	m.MountId = mountId
+
+	return nil
 }
