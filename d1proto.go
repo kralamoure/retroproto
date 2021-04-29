37fa484a5bc665f84fd1ff6dbc3b0e4207eaa2df
Raúl Garrido <me@happybydefault.com>
2021-04-29 17:34:14 -0400

    docs: change package description

--- d1proto.go
+++ d1proto.go
@@ -1,5 +1,5 @@
 // Package d1proto is a library that implements the network protocol of Dofus 1,
-// for communication between server and client. It defines the ID and structure
+// for communication between server and client. It defines the Id and structure
 // of the protocol messages, and it provides functionality for their
 // serialization and deserialization.
 package d1proto
